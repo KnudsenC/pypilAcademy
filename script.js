98d6f08969c3f76b9dc7fbd633cfbc58b373b45d
KnudsenC <91569545+KnudsenC@users.noreply.github.com>
2022-06-09 13:41:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+if(localStorage.getItem("ch0") == "print"){
+  document.getElementById("ch0").src = "print.html";
+  document.getElementsByTagName("h1")[0].innerHTML = "Python - The Print Statement - Article";
+  localStorage.setItem("ch0", "false");
+}
+
+if(localStorage.getItem("ch1") == "library"){
+  document.getElementById("ch1").src = "library.html";
+  document.getElementsByTagName("h1")[0].innerHTML = "Python - Variables - The Library - Task";
+  localStorage.setItem("ch1", "false");
+}
+
+function openSide() {
+  document.getElementById("sideNavID").style.width = "250px";
+  document.getElementById("sideNavID").style.borderWidth = "10px";
+}
+
+
+function closeSide() {
+  document.getElementById("sideNavID").style.width = "0";
+  document.getElementById("sideNavID").style.borderWidth = "0px";
+}
+
+function ch0Switch(inner) {
+  if(inner == true) {
+    localStorage.setItem("ch0", "print");
+    window.top.location.reload();
+  }
+  else if(inner == false) {
+    localStorage.setItem("ch0", "print");
+  }
+  else if(inner == "previous") {
+    localStorage.setItem("ch0", "print");
+    window.top.open('/chapter0/index.html','_self');
+  }
+}
+
+
+function ch1Switch(inner) {
+  if(inner == true) {
+    localStorage.setItem("ch1", "library");
+    window.top.location.reload();
+  }
+  else {
+    localStorage.setItem("ch1", "library");
+  }
+}
